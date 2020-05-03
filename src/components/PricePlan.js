e312638349dfe906c51614e649d7a03c3e3ad937
Ayodele Olalekan <longyarnz@gmail.com>
2020-05-03 15:42:02 +0100

    Add new pricing plans

--- src/components/PricePlan.js
+++ src/components/PricePlan.js
@@ -7,25 +7,25 @@ export default function PricePlan() {
   return (
     <div>
       <PricingTable highlightColor='#1976D2'>
-        <PricingSlot onClick={submit} buttonText='TRY IT FREE' title='FREE' priceText='$0/month'>
+        <PricingSlot highlighted buttonText='Current Plan' title='FREE' priceText='₦0/month'>
           <PricingDetail> <b>15</b> projects</PricingDetail>
           <PricingDetail> <b>5 GB</b> storage</PricingDetail>
           <PricingDetail> <b>5</b> users</PricingDetail>
           <PricingDetail strikethrough> <b>Time tracking</b></PricingDetail>
         </PricingSlot>
-        <PricingSlot highlighted onClick={submit} buttonText='SIGN UP' title='BASIC' priceText='$24/month'>
+        <PricingSlot onClick={submit} buttonText='UPGRADE' title='Bronze' priceText='₦24/month'>
           <PricingDetail> <b>35</b> projects</PricingDetail>
           <PricingDetail> <b>15 GB</b> storage</PricingDetail>
           <PricingDetail> <b>Unlimited</b> users</PricingDetail>
           <PricingDetail> <b>Time tracking</b></PricingDetail>
         </PricingSlot>
-        <PricingSlot onClick={submit} buttonText='SIGN UP' title='PROFESSIONAL' priceText='$99/month'>
+        <PricingSlot onClick={submit} buttonText='UPGRADE' title='Silver' priceText='₦99/month'>
           <PricingDetail> <b>100</b> projects</PricingDetail>
           <PricingDetail> <b>30 GB</b> storage</PricingDetail>
           <PricingDetail> <b>Unlimited</b> users</PricingDetail>
           <PricingDetail> <b>Time tracking</b></PricingDetail>
         </PricingSlot>
-        <PricingSlot onClick={submit} buttonText='SIGN UP' title='ENTERPRISE' priceText='$200/month'>
+        <PricingSlot onClick={submit} buttonText='UPGRADE' title='Gold' priceText='₦200/month'>
           <PricingDetail> <b>Unlimited</b> projects</PricingDetail>
           <PricingDetail> <b>75 GB</b> storage</PricingDetail>
           <PricingDetail> <b>Unlimited</b> users</PricingDetail>
