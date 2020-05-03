3c825fb39f48fec46ce00830a40c50f258674b47
Ayodele Olalekan <longyarnz@gmail.com>
2020-05-03 15:48:03 +0100

    Create upgrade methods

--- src/components/PricePlan.js
+++ src/components/PricePlan.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { PricingTable, PricingSlot, PricingDetail } from 'react-pricing-table';
 
 export default function PricePlan() {
-  const submit = () => console.log('Plan submitted!');
+  const switchToBronze = () => console.log('Welcome to Bronze Plan');
+  const switchToSilver = () => console.log('Welcome to Silver Plan');
+  const switchToGold = () => console.log('Welcome to Gold Plan');
 
   return (
     <div>
@@ -13,19 +15,19 @@ export default function PricePlan() {
           <PricingDetail> <b>5</b> users</PricingDetail>
           <PricingDetail strikethrough> <b>Time tracking</b></PricingDetail>
         </PricingSlot>
-        <PricingSlot onClick={submit} buttonText='UPGRADE' title='Bronze' priceText='₦24/month'>
+        <PricingSlot onClick={switchToBronze} buttonText='UPGRADE' title='Bronze' priceText='₦24/month'>
           <PricingDetail> <b>35</b> projects</PricingDetail>
           <PricingDetail> <b>15 GB</b> storage</PricingDetail>
           <PricingDetail> <b>Unlimited</b> users</PricingDetail>
           <PricingDetail> <b>Time tracking</b></PricingDetail>
         </PricingSlot>
-        <PricingSlot onClick={submit} buttonText='UPGRADE' title='Silver' priceText='₦99/month'>
+        <PricingSlot onClick={switchToSilver} buttonText='UPGRADE' title='Silver' priceText='₦99/month'>
           <PricingDetail> <b>100</b> projects</PricingDetail>
           <PricingDetail> <b>30 GB</b> storage</PricingDetail>
           <PricingDetail> <b>Unlimited</b> users</PricingDetail>
           <PricingDetail> <b>Time tracking</b></PricingDetail>
         </PricingSlot>
-        <PricingSlot onClick={submit} buttonText='UPGRADE' title='Gold' priceText='₦200/month'>
+        <PricingSlot onClick={switchToGold} buttonText='UPGRADE' title='Gold' priceText='₦200/month'>
           <PricingDetail> <b>Unlimited</b> projects</PricingDetail>
           <PricingDetail> <b>75 GB</b> storage</PricingDetail>
           <PricingDetail> <b>Unlimited</b> users</PricingDetail>
